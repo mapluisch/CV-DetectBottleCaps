a779b678fb5f1479d439f0cec6e18a150106291d
Martin Pluisch <31780571+mapluisch@users.noreply.github.com>
2020-12-31 16:35:44 +0100

    refactoring and general cleanup
    - added "global" printDebug function instead of checking DEBUG for every print message; also, the print messages now look a lot better with truncated time-strings upfront
    - made debug-output a lot richer and more in-depth
    - added frame-skip option for live-video feed
    - renamed some input-arguments to be more precise/specific
    - added README

--- Cheers/README.md
+++ Cheers/README.md
@@ -1,1 +1,31 @@
-Quick setup: install all yolov5 requirements using pip3 install -r requirements.txt -t ../yolo_dependencies
+# Cheers - "Detect The Bottle Caps" (CV20/21)
+**Cheers** is a tiny Python3 script, which 
+
+ 1. reads some input video file
+ 2. detects and extracts a still frame
+ 3. runs inference on the still frame
+ 4. detects and annotates any matching bottle caps (face-down/face-up/deformed)
+ 
+ I've trained my model using YOLOv4-tiny using ~500 images for training.
+ ## Dependencies
+ OpenCV-Python
+ Python3
+ ## Install / Run
+Simply clone this repo or download + unzip. Then, simply run
+`python3 cheers.py --video some_vid.mp4`
+or
+`python3 cheers.py --image some_img.jpg`
+and you'll get an annotated still frame in ./Output/Annotated Frames/ after approx. 0.2s for input images and approx. 0.5-1.0s for input videos. 
+**Cheers** can also be used to live-annotate a passed-in video. You can also specify your own confidence- and non-max-suppresion-thresholds; please take a look at the parameters below.
+
+### Parameters
+```
+--verbose 				print extensive information per step during runtime
+--video					path to video file (to first detect a still frame from and then run inference on)
+--image					path to image file (to directly run inference on)
+--conf-thres			confidence threshold [0.0-1.0] whether or not a detected object is a bottle cap
+--nms-thres				non-max-suppression threshold [0.0-1.0] used to select the best bounding box per detected object
+--show-result			immediately show the annotated output frame using cv2.imshow
+--save-stillframe		save the detected still frame (default = false)
+--live					show live-annotation of the passed in video-file, frame-by-frame
+```
--- Cheers/cheers.py
+++ Cheers/cheers.py
@@ -1,49 +1,57 @@
 import sys
-sys.path.insert(0, "./yolo_dependencies")
-sys.path.insert(0, "./yolov5")
 import os
 import cv2
 import time
 import argparse
 import numpy as np
 
-DEBUG = False
+VERBOSE = False
 FRAME_POSITION = cv2.CAP_PROP_POS_FRAMES
 FRAME_COUNT = cv2.CAP_PROP_FRAME_COUNT
 START_FRAME_FACTOR = 1/3
 END_FRAME_FACTOR = 2/3
+
 STILLFRAME_FOLDER = './Output/Still Frames/'
 ANNOTATED_OUTPUT_FOLDER = './Output/Annotated Frames/'
+
+CONFIG_DIR = './YOLOv4/Config/'
+CONFIG_NAME = 'v4tiny_6000.cfg'
+
+WEIGHTS_DIR = './YOLOv4/Weights/'
+WEIGHTS_NAME = 'v4tiny_6000.weights'
 
 NON_MAX_SUPRESSION_THRESHOLD = 0.5
 CONF_THRESHOLD = 0.5
 
 CLASS_COLORS = [(255, 101, 59), (19, 201, 225), (53, 159, 7)]
+CLASS_NAMES = ["face-down", "face-up", "deformed"]
 
+def printDebug(message, forcePrint=False):
+    if VERBOSE or forcePrint: print("-- [%.3f s] %s --" % ((time.time() - start_time), message))
 
 def detect_the_bottle_caps():
-    if os.path.exists(opt.input) or os.path.exists(opt.input_img):
-        # ----- Live Video Inference
-        if(opt.video_inference):
-            if DEBUG: print("-- starting live video inference using %s --" % opt.input)
-            video_inference(video=opt.input)
+    if os.path.exists(opt.video) or os.path.exists(opt.image):
+        # ----- Live Video Inference -----
+        if(opt.live):
+            printDebug("starting live video inference using %s" % opt.video, forcePrint=True)
+            video_inference(opt.video)
 
-        # ----- Image Inference
-        # ----- Still Frame Detection + Inference / Input Image Inference
+        # ----- Image Inference -----
+        # Still Frame Detection + Inference / Input Image Inference
         else:
-            should_detect_stillframe = os.path.exists(opt.input)
-            if(should_detect_stillframe): still_frame = get_still_frame_from_video(opt.input)
-            else: still_frame = cv2.imread(opt.input_img)
-            if DEBUG: print("-- inference started after %s seconds --" % (time.time() - start_time))
+            should_detect_stillframe = os.path.exists(opt.video)
+            if(should_detect_stillframe): still_frame = get_still_frame_from_video(opt.video)
+            else: still_frame = cv2.imread(opt.image)
+            printDebug("inference started")
             annotated_image = image_inference(still_frame)
-            if DEBUG: print("-- inference ended after %s seconds --" % (time.time() - start_time))
-            if(opt.view_img):
-                cv2.imshow("Cheers - Annotated Output", annotated_image)
-                cv2.waitKey()
-            filename = opt.input if (should_detect_stillframe) else opt.input_img
+            printDebug("inference ended")
+            filename = opt.video if (should_detect_stillframe) else opt.image
             output_filepath = ANNOTATED_OUTPUT_FOLDER + filename + "_annotated.jpg"
             cv2.imwrite(output_filepath, annotated_image)
-            if DEBUG: print("-- saved annotated output image after %s seconds --" % (time.time() - start_time))
+            printDebug("successfully saved an annotated frame [%s]" % output_filepath, forcePrint=True)
+            if(opt.show_result):
+                cv2.imshow("Cheers - Annotated Output", annotated_image)
+                cv2.waitKey()
     else:
         print("Please specify a correct file path to your video/image file.")
 
@@ -51,7 +59,7 @@ def detect_the_bottle_caps():
 def get_still_frame_from_video(video_filepath):
     # load OpenCV VideoCapture on specified video
     vid = cv2.VideoCapture(video_filepath)
-    if DEBUG: print("-- opened video after %s seconds --" % (time.time() - start_time))
+    printDebug("opened video file")
     # get total num of frames
     total_frames    = int(cv2.VideoCapture.get(vid, FRAME_COUNT))
     # calculate start and end-frame for further inspection based on the total amount of frames
@@ -60,11 +68,11 @@ def get_still_frame_from_video(video_filepath):
     end_frame       = int(total_frames*END_FRAME_FACTOR)
     fps             = int(vid.get(cv2.CAP_PROP_FPS))
 
-    if DEBUG: print("-- calculated frame range after %s seconds --" % (time.time() - start_time))
+    printDebug("calculated frame range")
     # "manually" read and store first frame, so that frame i+1 can check against last_frame (= frame i)
     vid.set(FRAME_POSITION, start_frame)
     ret, last_frame = vid.read()
-    if DEBUG: print("-- grabbed first frame after %s seconds --" % (time.time() - start_time))    
+    printDebug("grabbed first frame")    
     
     detected_still_frame = last_frame
     min_absdiff = float('inf')
@@ -84,89 +92,97 @@ def get_still_frame_from_video(video_filepath):
 
         last_frame = frame
     
-    if DEBUG: print("-- detected still frame after %s seconds --" % (time.time() - start_time))    
+    printDebug("detected still frame")    
     
     if(opt.save_stillframe):
         filename = STILLFRAME_FOLDER + f'{video_filepath}_stillframe.jpg'  
         cv2.imwrite(filename, detected_still_frame)
-        if DEBUG: print("-- saved still frame after %s seconds --" % (time.time() - start_time))
+        printDebug("saved still frame")
 
     return detected_still_frame
 
 def image_inference(image):
-    net = cv2.dnn_DetectionModel('./YOLOv4/Config/mpluis2s_yv4.cfg', './YOLOv4/Weights/mpluis2s_yv4.weights')
+    # init neural net-model using my yolov4 config and self-trained weights
+    net = cv2.dnn_DetectionModel((CONFIG_DIR + CONFIG_NAME), (WEIGHTS_DIR + WEIGHTS_NAME))
     net.setInputScale(1/500)
-    net.setInputSize((960,540))
-    
-
+    net.setInputSize((960,540))    
+    # set preferred backend and target to run the inference on
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT);
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU);
-
+    printDebug("initialized detection model")
+    printDebug("starting bottle cap detection")
+    # run opencv's detect function on the passed-in image, using the specified confidence and non-max-supression threshs
     classIds, confidences, boxes = net.detect(image, opt.conf_thres, opt.nms_thres)
-
     # get amount of distinct class objects
     classList = list(classIds)
     bottlecaps_facedown = classList.count(0)
     bottlecaps_faceup = classList.count(1)
     bottlecaps_deformed = classList.count(2)
 
+    printDebug("detected %s bottle caps in total [%i %s, %i %s, %i %s]" % (len(boxes), bottlecaps_facedown, CLASS_NAMES[0], bottlecaps_faceup, CLASS_NAMES[1], bottlecaps_deformed, CLASS_NAMES[2]))
+    # draw bounding boxes around detected bottle caps
+    box_linethickness = 4
+    printDebug("annotation started")
     for box in range(0, len(boxes)):
         x, y = boxes[box][0:2]
         w, h = boxes[box][2:4]
         color = CLASS_COLORS[classIds[box][0]]
-        cv2.rectangle (image, (x,y), (x+w, y+h), color, 2)
+        cv2.rectangle (image, (x,y), (x+w, y+h), color, box_linethickness)
+        printDebug("annotation: bottle cap %s at position (%s,%s) with confidence %.2f" % (CLASS_NAMES[classIds[box][0]], x, y, confidences[box]))
+    printDebug("annotation ended")
+    # -- draw top-left verbose info-output --
+    
     
     # draw transparent rectangle for better text-readability (source, but adapted by me: https://stackoverflow.com/questions/56472024/how-to-change-the-opacity-of-boxes-cv2-rectangle)
     x, y, w, h = 0, 0, 426, 150
     sub_img = image[y:y+h, x:x+w]
     black_rect = np.ones(sub_img.shape, dtype=np.uint8) * 0
     res = cv2.addWeighted(sub_img, 0.25, black_rect, 0.5, 1.0)
-    # Putting the image back to its position
     image[y:y+h, x:x+w] = res
-    # draw actual text
+    
+    # draw text info
     cv2.putText(image, str(bottlecaps_facedown) + " bottle caps face-down", (0,25), cv2.FONT_HERSHEY_DUPLEX, 1, CLASS_COLORS[0], 2)
     cv2.putText(image, str(bottlecaps_faceup) + " bottle caps face-up", (0,60), cv2.FONT_HERSHEY_DUPLEX, 1, CLASS_COLORS[1], 2)
     cv2.putText(image, str(bottlecaps_deformed) + " bottle caps deformed", (0,95), cv2.FONT_HERSHEY_DUPLEX, 1, CLASS_COLORS[2], 2)
-    # draw separator line
-    cv2.line(image, (0, 108), (424, 108), (175,175,175), 2)
     cv2.putText(image, str(bottlecaps_facedown + bottlecaps_faceup + bottlecaps_deformed) + " bottle caps in total", (0,140), cv2.FONT_HERSHEY_DUPLEX, 1, (255,255,255), 2)
+    # draw separator line (between total amount and classes)
+    cv2.line(image, (0, 108), (424, 108), (175,175,175), 2)
 
     return image
 
 def video_inference(video):
-    cap = cv2.VideoCapture(video)
-    while True:
-        flag, frame = cap.read()
-        if flag:
-            # The frame is ready and already captured
-            annotated_frame = image_inference(frame)
-            cv2.imshow('video', annotated_frame)
-            pos_frame = cap.get(FRAME_POSITION)
+    vid = cv2.VideoCapture(video)
+    total_frames    = int(cv2.VideoCapture.get(vid, FRAME_COUNT))
+    frameskip       = 1
 
+    for i in range(0, total_frames, 1+frameskip):
+        # skip to frame i
+        vid.set(FRAME_POSITION, i)
+        # read still image from video frame, check ret (bool) if frame is correctly read or not
+        ret, frame = vid.read()
+
+        if (ret):
+            annotated_frame = image_inference(frame)
+            cv2.imshow('Cheers - Video Inference', annotated_frame)          
+        
         if cv2.waitKey(1) == 27:
             break
-        if cap.get(FRAME_POSITION) == cap.get(FRAME_COUNT):
-            # If the number of captured frames is equal to the total number of frames,
-            # we stop
-            break
-
 
 
 if __name__ == "__main__":
     start_time = time.time()
     parser = argparse.ArgumentParser()
-    parser.add_argument('--debug', action='store_true', help='print some debug info during runtime')
-    parser.add_argument('--input' , type=str, default='', help='input video file to scan for bottle caps')
-    parser.add_argument('--input-img' , type=str, default='', help='input image file to directly scan for bottle caps')
-    parser.add_argument('--conf-thres', type=float, default=CONF_THRESHOLD, help='object confidence threshold')
-    parser.add_argument('--nms-thres', type=float, default=NON_MAX_SUPRESSION_THRESHOLD, help='non max suppression threshold')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-stillframe', action='store_true', help='save detected still-frames in the corresponding subfolder')
-    parser.add_argument('--video-inference', action='store_true', help='use the video input and directly run it through yolov5')
+    parser.add_argument('--verbose', action='store_true',                                       help='print verbose info during runtime')
+    parser.add_argument('--video' , type=str, default='',                                       help='input video file to extract a still frame and scan for bottle caps')
+    parser.add_argument('--image' , type=str, default='',                                       help='input image file to directly scan for bottle caps')
+    parser.add_argument('--conf-thres', type=float, default=CONF_THRESHOLD,                     help='object confidence threshold')
+    parser.add_argument('--nms-thres', type=float, default=NON_MAX_SUPRESSION_THRESHOLD,        help='non max suppression threshold')
+    parser.add_argument('--show-result', action='store_true',                                   help='display results')
+    parser.add_argument('--save-stillframe', action='store_true',                               help='save detected still-frames in the corresponding subfolder')
+    parser.add_argument('--live', action='store_true',                                          help='live-annotation of the specified input video, frame-by-frame')
     opt = parser.parse_args()
     
-    DEBUG = opt.debug
+    VERBOSE = opt.verbose
     
-    if(opt.input or opt.input_img):
+    if(opt.video or opt.image):
         detect_the_bottle_caps()
-    print("-- total runtime: %s seconds --" % (time.time() - start_time))
