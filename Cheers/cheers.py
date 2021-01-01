fe323d3972000a9bc02fc6dbb8a3b741f38fd33a
Martin Pluisch <martin.pluisch@icloud.com>
2021-01-01 12:36:07 +0100

    added info-header to bounding box-annotation + more
    - added some code comments
    - added option to show confidence values in the annotated result (--show-conf)
    - updated README

--- Cheers/cheers.py
+++ Cheers/cheers.py
@@ -10,6 +10,7 @@ FRAME_POSITION = cv2.CAP_PROP_POS_FRAMES
 FRAME_COUNT = cv2.CAP_PROP_FRAME_COUNT
 START_FRAME_FACTOR = 1/3
 END_FRAME_FACTOR = 2/3
+LIVE_FRAMESKIP = 0
 
 STILLFRAME_FOLDER = './Output/Still Frames/'
 ANNOTATED_OUTPUT_FOLDER = './Output/Annotated Frames/'
@@ -22,33 +23,52 @@ WEIGHTS_NAME = 'v4tiny_15000.weights'
 
 NON_MAX_SUPRESSION_THRESHOLD = 0.5
 CONF_THRESHOLD = 0.5
+ANNOTATION_LINE_THICKNESS = 4
+
 
 CLASS_COLORS = [(255, 101, 59), (19, 201, 225), (53, 159, 7)]
 CLASS_NAMES = ["face-down", "face-up", "deformed"]
 
-def printDebug(message, forcePrint=False):
+def print_verbose(message, forcePrint=False):
     if VERBOSE or forcePrint: print("-- [%.3f s] %s --" % ((time.time() - start_time), message))
+
+def clean_filename(filename):
+    try:
+        # filter path information out of passed in filename, e.g. if the specified file is in a different folder
+        components = filename.split('/')
+        return components[len(components)-1]
+    except ValueError:
+        print_verbose("could not clean filename")
+        return filename
 
 def detect_the_bottle_caps():
     if os.path.exists(opt.video) or os.path.exists(opt.image):
         # ----- Live Video Inference -----
         if(opt.live):
-            printDebug("starting live video inference using %s" % opt.video, forcePrint=True)
+            print_verbose("starting live video inference using %s" % opt.video, forcePrint=True)
             video_inference(opt.video)
 
         # ----- Image Inference -----
         # Still Frame Detection + Inference / Input Image Inference
         else:
+            # either detect a still frame from the video or directly use the passed-in --image file
             should_detect_stillframe = os.path.exists(opt.video)
             if(should_detect_stillframe): still_frame = get_still_frame_from_video(opt.video)
             else: still_frame = cv2.imread(opt.image)
-            printDebug("inference started")
+            
+            # start inference on the detected/supplied still frame
+            print_verbose("inference started")
             annotated_image = image_inference(still_frame)
-            printDebug("inference ended")
+            print_verbose("inference ended")
+
+            # save the annotated result in the annotated output folder
             filename = opt.video if (should_detect_stillframe) else opt.image
+            filename = clean_filename(filename)
             output_filepath = ANNOTATED_OUTPUT_FOLDER + filename + "_annotated.jpg"
             cv2.imwrite(output_filepath, annotated_image)
-            printDebug("successfully saved an annotated frame [%s]" % output_filepath, forcePrint=True)
+            print_verbose("successfully saved an annotated frame [%s]" % output_filepath, forcePrint=True)
+
+            # directly show the results if --show-result is being used
             if(opt.show_result):
                 cv2.imshow("Cheers - Annotated Output", annotated_image)
                 cv2.waitKey()
@@ -59,45 +79,48 @@ def detect_the_bottle_caps():
 def get_still_frame_from_video(video_filepath):
     # load OpenCV VideoCapture on specified video
     vid = cv2.VideoCapture(video_filepath)
-    printDebug("opened video file")
+    print_verbose("opened video file")
+
     # get total num of frames
     total_frames    = int(cv2.VideoCapture.get(vid, FRAME_COUNT))
+    
     # calculate start and end-frame for further inspection based on the total amount of frames
-    # in this case: inspection starts at 1/4th of the videos length and ends at 3/4ths, skipping 50% of "unnecessary" video footage
     start_frame     = int(total_frames*START_FRAME_FACTOR)
     end_frame       = int(total_frames*END_FRAME_FACTOR)
     fps             = int(vid.get(cv2.CAP_PROP_FPS))
+    print_verbose("calculated frame range")
 
-    printDebug("calculated frame range")
     # "manually" read and store first frame, so that frame i+1 can check against last_frame (= frame i)
     vid.set(FRAME_POSITION, start_frame)
     ret, last_frame = vid.read()
-    printDebug("grabbed first frame")    
+    print_verbose("grabbed first frame")    
     
+    # pre-set detected-still frame to the initially read frame and its absolute difference to the previous one (infinity, since there is no previous frame)
     detected_still_frame = last_frame
     min_absdiff = float('inf')
 
     # check one frame per every two seconds of video
-
+    print_verbose("looking for a still frame")
     for i in range(start_frame+1, end_frame, fps*2):
         # skip to frame i
         vid.set(FRAME_POSITION, i)
         # read still image from video frame, check ret (bool) if frame is correctly read or not
         ret, frame = vid.read()
-
+        # calculate absolute difference between frame (x-1) and (x)
         diff = cv2.absdiff(last_frame, frame)
+        # overwrite detected_still_frame if new lowest difference has been found
         if(diff.sum() < min_absdiff):
             detected_still_frame = frame
             min_absdiff = diff.sum()
-
+        # set frame (x-1) to (x) for the next iteration
         last_frame = frame
+    print_verbose("detected a still frame")    
     
-    printDebug("detected still frame")    
-    
+    # save the detected still frame if --save-stillframe is being used
     if(opt.save_stillframe):
         filename = STILLFRAME_FOLDER + f'{video_filepath}_stillframe.jpg'  
         cv2.imwrite(filename, detected_still_frame)
-        printDebug("saved still frame")
+        print_verbose("saved still frame")
 
     return detected_still_frame
 
@@ -106,30 +129,39 @@ def image_inference(image):
     net = cv2.dnn_DetectionModel((CONFIG_DIR + CONFIG_NAME), (WEIGHTS_DIR + WEIGHTS_NAME))
     net.setInputScale(1/500)
     net.setInputSize((960,540))    
+    
     # set preferred backend and target to run the inference on
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT);
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU);
-    printDebug("initialized detection model")
-    printDebug("starting bottle cap detection")
+    print_verbose("initialized detection model")
+    print_verbose("starting bottle cap detection")
+    
     # run opencv's detect function on the passed-in image, using the specified confidence and non-max-supression threshs
     classIds, confidences, boxes = net.detect(image, opt.conf_thres, opt.nms_thres)
+    
     # get amount of distinct class objects
     classList = list(classIds)
     bottlecaps_facedown = classList.count(0)
     bottlecaps_faceup = classList.count(1)
     bottlecaps_deformed = classList.count(2)
-
-    printDebug("detected %s bottle caps in total [%i %s, %i %s, %i %s]" % (len(boxes), bottlecaps_facedown, CLASS_NAMES[0], bottlecaps_faceup, CLASS_NAMES[1], bottlecaps_deformed, CLASS_NAMES[2]))
+    print_verbose("detected %s bottle caps in total [%i %s, %i %s, %i %s]" % (len(boxes), bottlecaps_facedown, CLASS_NAMES[0], bottlecaps_faceup, CLASS_NAMES[1], bottlecaps_deformed, CLASS_NAMES[2]))
+    
     # draw bounding boxes around detected bottle caps
-    box_linethickness = 4
-    printDebug("annotation started")
+    print_verbose("annotation started")
     for box in range(0, len(boxes)):
         x, y = boxes[box][0:2]
         w, h = boxes[box][2:4]
         color = CLASS_COLORS[classIds[box][0]]
-        cv2.rectangle (image, (x,y), (x+w, y+h), color, box_linethickness)
-        printDebug("annotation: bottle cap %s at position (%s,%s) with confidence %.2f" % (CLASS_NAMES[classIds[box][0]], x, y, confidences[box]))
-    printDebug("annotation ended")
+        # draw bounding box
+        cv2.rectangle (image, (x,y), (x+w, y+h), color, ANNOTATION_LINE_THICKNESS)
+        # draw info header
+        header_height = 16
+        cv2.rectangle (image, (x,y), (x+w, y+header_height), color, -1)
+        cv2.putText(image, CLASS_NAMES[classIds[box][0]], (x,y+header_height-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
+        # draw confidence, if --show-conf is being used
+        if (opt.show_conf): cv2.putText(image, "%.2f" % confidences[box], (x,y+h-5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,0), 2)
+        print_verbose("annotation: bottle cap %s at position (%s,%s) with confidence %.2f" % (CLASS_NAMES[classIds[box][0]], x, y, confidences[box]))
+    print_verbose("annotation ended")
     # -- draw top-left verbose info-output --
     
     
@@ -140,37 +172,38 @@ def image_inference(image):
     res = cv2.addWeighted(sub_img, 0.25, black_rect, 0.5, 1.0)
     image[y:y+h, x:x+w] = res
     
-    # draw text info
+    # draw text info and separator line (between total amount and classes)
     cv2.putText(image, str(bottlecaps_facedown) + " bottle caps face-down", (0,25), cv2.FONT_HERSHEY_DUPLEX, 1, CLASS_COLORS[0], 2)
     cv2.putText(image, str(bottlecaps_faceup) + " bottle caps face-up", (0,60), cv2.FONT_HERSHEY_DUPLEX, 1, CLASS_COLORS[1], 2)
     cv2.putText(image, str(bottlecaps_deformed) + " bottle caps deformed", (0,95), cv2.FONT_HERSHEY_DUPLEX, 1, CLASS_COLORS[2], 2)
     cv2.putText(image, str(bottlecaps_facedown + bottlecaps_faceup + bottlecaps_deformed) + " bottle caps in total", (0,140), cv2.FONT_HERSHEY_DUPLEX, 1, (255,255,255), 2)
-    # draw separator line (between total amount and classes)
     cv2.line(image, (0, 108), (424, 108), (175,175,175), 2)
 
     return image
 
 def video_inference(video):
+    # read video file
     vid = cv2.VideoCapture(video)
-    total_frames    = int(cv2.VideoCapture.get(vid, FRAME_COUNT))
-    frameskip       = 1
+    total_frames = int(cv2.VideoCapture.get(vid, FRAME_COUNT))
 
-    for i in range(0, total_frames, 1+frameskip):
+    # use every 1+LIVE_FRAMESKIP frame to read and run inference on
+    for i in range(0, total_frames, 1+LIVE_FRAMESKIP):
         # skip to frame i
         vid.set(FRAME_POSITION, i)
         # read still image from video frame, check ret (bool) if frame is correctly read or not
         ret, frame = vid.read()
-
+        # if frame is correctly read, run inference and immediately show the result
         if (ret):
             annotated_frame = image_inference(frame)
             cv2.imshow('Cheers - Video Inference', annotated_frame)          
-        
+        # press ESC to quit prematurely
         if cv2.waitKey(1) == 27:
             break
 
 
 if __name__ == "__main__":
     start_time = time.time()
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--verbose', action='store_true',                                       help='print verbose info during runtime')
     parser.add_argument('--video' , type=str, default='',                                       help='input video file to extract a still frame and scan for bottle caps')
@@ -178,6 +211,7 @@ if __name__ == "__main__":
     parser.add_argument('--conf-thres', type=float, default=CONF_THRESHOLD,                     help='object confidence threshold')
     parser.add_argument('--nms-thres', type=float, default=NON_MAX_SUPRESSION_THRESHOLD,        help='non max suppression threshold')
     parser.add_argument('--show-result', action='store_true',                                   help='display results')
+    parser.add_argument('--show-conf', action='store_true',                                     help='display the confidence values in the annotated result')
     parser.add_argument('--save-stillframe', action='store_true',                               help='save detected still-frames in the corresponding subfolder')
     parser.add_argument('--live', action='store_true',                                          help='live-annotation of the specified input video, frame-by-frame')
     opt = parser.parse_args()
--- README.md
+++ README.md
@@ -5,27 +5,35 @@
  2. detects and extracts a still frame
  3. runs inference on the still frame
  4. detects and annotates any matching bottle caps (face-down/face-up/deformed)
- 
+
  I've trained my model using YOLOv4-tiny using ~500 images for training.
  ## Dependencies
  OpenCV-Python
+
  Python3
+
  ## Install / Run
 Simply clone this repo or download + unzip. Then, simply run
+
 `python3 cheers.py --video some_vid.mp4`
+
 or
+
 `python3 cheers.py --image some_img.jpg`
 and you'll get an annotated still frame in ./Output/Annotated Frames/ after approx. 0.2s for input images and approx. 0.5-1.0s for input videos. 
+
 **Cheers** can also be used to live-annotate a passed-in video. You can also specify your own confidence- and non-max-suppresion-thresholds; please take a look at the parameters below.
 
-### Parameters
+ ## Arguments
+
 ```
---verbose 				print extensive information per step during runtime
---video					path to video file (to first detect a still frame from and then run inference on)
---image					path to image file (to directly run inference on)
---conf-thres			confidence threshold [0.0-1.0] whether or not a detected object is a bottle cap
---nms-thres				non-max-suppression threshold [0.0-1.0] used to select the best bounding box per detected object
---show-result			immediately show the annotated output frame using cv2.imshow
+--verbose 					print extensive information per step during runtime
+--video							path to video file (to first detect a still frame from and then run inference on)
+--image							path to image file (to directly run inference on)
+--conf-thres				confidence threshold [0.0-1.0] whether or not a detected object is a bottle cap
+--nms-thres					non-max-suppression threshold [0.0-1.0] used to select the best bounding box per detected object
+--show-result				immediately show the annotated output frame using cv2.imshow
+--show-conf					display the confidence values in the annotated result
 --save-stillframe		save the detected still frame (default = false)
---live					show live-annotation of the passed in video-file, frame-by-frame
+--live							show live-annotation of the passed in video-file, frame-by-frame
 ```
